6850744f9575babc26d141dddc1f14a02744d03b
p11pp7 <48938634+p11pp7@users.noreply.github.com>
2019-05-10 13:14:16 +0500

    Add files via upload

---
+++ start_server.bat
@@ -0,0 +1,12 @@
+srcds.exe
+-game csgo
+-console
+-usercon
+-maxplayers_override 20
+-tickrate 64
++map de_bank
+-port 27015
++game_type 0
++game_mode 0
+-secure
++sv_lan 0
---
+++ update_csgo.bat
@@ -0,0 +1,5 @@
+steamcmd.exe
++login anonymous
++force_install_dir F:\server\server_csgo
++app_update 740 validate
++quit
